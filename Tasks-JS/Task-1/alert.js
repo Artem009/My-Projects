754538788e60d01e902aee11cb31d2414591a141
Artem009 <artemstrigkov@gmail.com>
2017-12-12 23:36:37 +0300

    first commit

--- Tasks-JS/Task-1/alert.js
+++ Tasks-JS/Task-1/alert.js
@@ -1,1 +1,13 @@
 alert("«Я – JavaScript!».");
+alert("Привет"+" Мир!!!");
+alert("Мир!!!");
+var admin, name;
+name= "Василий";
+admin=name;
+alert(admin);
+var earth;
+earth="Земля"
+document.write(earth);
+var vizitor;
+vizitor="Петя";
+document.write(vizitor);
